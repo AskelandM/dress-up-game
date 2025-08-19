7fe6720b81f48730945677942fbd1f0f7a1ceca3
Morgan Askeland <morganaskeland@gmail.com>
2025-08-19 16:06:33 -0500

    Initial commit

---
+++ Outfit.cs
@@ -0,0 +1,8 @@
+public class Outfit
+{
+    public string Hair { get; set; } = "2020s";
+    public string Face { get; set; } = "2020s";
+    public string Shirt { get; set; } = "red flannel";
+    public string Pants { get; set; } = "black skinny jeans";
+    public string Shoes { get; set; } = "black platforms";
+}
